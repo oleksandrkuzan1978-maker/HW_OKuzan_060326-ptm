8a527edbfa74f78423ca24baed45bbac2e65d9a4
kuzan <oleksandrkuzan1978@gmail.com>
2026-09-04 19:37:26 +0200

    Flask?HW_2[D[D[3~[3~[3~[3~[C[C[C[C[3~[C[[3~[3~[C[C[C[C[C[C[HW_2[D[C Flask

---
+++ HW_Flask/HW_2/json_text_tests.py
@@ -0,0 +1,61 @@
+tests = [
+"""{
+    "name": "John Doe",
+    "age": 20,
+    "email": "john.doe@example.com",
+    "is_employed": true,
+    "address": {
+        "city": "New York",
+        "street": "5th Avenue",
+        "house_number": 123
+    }
+}""",
+
+"""{
+    "name": "John Doe",
+    "age": 120,
+    "email": "john.doe@example.com",
+    "is_employed": true,
+    "address": {
+        "city": "New York",
+        "street": "5th Avenue",
+        "house_number": 123
+    }
+}""",
+
+"""{
+    "name": "John Doe",
+    "age": 20,
+    "email": "john.doe@example.com",
+    "is_employed": true,
+    "address": {
+        "city": "New York",
+        "street": "5th Avenue",
+        "house_number": 123.78
+    }
+}""",
+
+"""{
+    "name": "John Doe",
+    "age": 20,
+    "email": "john.doeexample.com",
+    "is_employed": true,
+    "address": {
+        "city": "New York",
+        "street": "5th Avenue",
+        "house_number": 123
+    }
+}""",
+
+"""{
+    "name": "John1 Doe",
+    "age": 20,
+    "email": "john.doe@example.com",
+    "is_employed": true,
+    "address": {
+        "city": "New York",
+        "street": "5th Avenue",
+        "house_number": 123
+    }
+}"""
+]
